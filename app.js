9ac53d43edd060ac97467f5b258716d7dd270af9
Satyam Singh Payak <125729215+satyamsingh91@users.noreply.github.com>
2025-02-02 23:08:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 // app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
 
