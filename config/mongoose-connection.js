e30a005e6b8a289644e37908e2c21b29c985607d
satyam singh payak <payaksatyamsingh@gmail.com>
2025-02-03 09:46:34 +0530

    project complete

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterestclone")
+mongoose.connect(process.env.MONGO_URI)
  const db =mongoose.connection;
 
  db.on("open",function(){
